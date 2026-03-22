09fcebc3479e250807855296afa9a8fc2609abe7
jbocanegral-hub <jbocanegral@unal.edu.co>
2026-03-22 16:04:53 -0500

    Añadir switch para el menu de inicio y el menu del admin

---
+++ GestorParking/src/Admin.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Admin {
+    public void accionAdmin (int accion){
+        /*Scanner lector = new Scanner(System.in);
+        
+        do {
+            System.out.print("Seleccione una opción: ");
+            System.out.println("[1] Cambiar tarifas");
+            System.out.println("[2] Registrar trabajador");
+            System.out.println("[3] Cambiar contraseña");
+            System.out.println("[4] Salir");
+            accion = lector.nextInt();
+            
+            switch (accion) {
+                case 1:
+                    System.out.println("--- Cambiar tarifas ---");
+                    cambiarTarifas();
+                    break;
+                    
+                case 2:
+                    System.out.println("--- Registrar trabajador ---");
+                    registrarTrabajador();
+                    break;
+                    
+                case 3:
+                    System.out.println("--- Cambiar contraseña ---");
+                    cambiarContraseña();
+                    break;
+                    
+                case 4:
+                    System.out.println("¡Hasta luego!");
+                    break;
+                    
+                default:
+                    System.out.println("Opción no válida. Intente nuevamente.");
+            }
+            
+        } while (accion != 4);
+        
+        */
+    }
+    
+}
--- GestorParking/src/App.java
+++ GestorParking/src/App.java
@@ -13,13 +13,23 @@ public class App {
         int n = 3000;
         Parqueadero p1 = new Parqueadero(n, nombre);
 
-
-
-        System.out.println("Ingrese el tipo de usuario");
-        int usuario = lector.nextInt();
-
+        int tipoUsuario = 0;
+        /*do {
+            System.out.println("\n=== SELECCIONE TIPO DE USUARIO ===");
+            System.out.println("[1] Administrador");
+            System.out.println("[2] Empleado");
+            System.out.println("[3] Cliente");
+            tipoUsuario = lector.nextInt();
+            
+            if (tipoUsuario < 1 || tipoUsuario > 3) {
+                System.out.println("pción no válida. Intente nuevamente.");
+            }
+            
+        } while (tipoUsuario < 1 || tipoUsuario > 3);*/
+        
         Administrador A1 = new Administrador("Alberto", 1010);
-        switch (usuario) {
+
+        switch (tipoUsuario) {
             case 1:
                 
                 System.out.println("Bienvenido Administrador \n Ingrese su contraseña");
@@ -32,10 +42,34 @@ public class App {
                     System.out.println("Contraseña incorrecta");
                     contrasena = lector.nextInt();
                 }
-                System.out.print("Por favor ingrese una nueva tarifa: ");
-                
-                int nuevatarifa = lector.nextInt();
-                A1.cambiarTarifa(p1, nuevatarifa);
+                /*
+                int accion = 0;
+                boolean opcionValida = false;
+
+                while (!opcionValida) {
+                    System.out.println("Que desea hacer [1] Cambiar tarifas [2] Registrar Trabajador [3] Cambiar Contraseña");
+                    accion = lector.nextInt();
+                    
+                    if (accion == 1 || accion == 2 || accion == 3) {
+                        opcionValida = true;
+                        Admin ad = new Admin();
+                        ad.accionAdmin(accion); 
+                    } else {
+                        System.out.println("Opción no válida. Intente nuevamente.");
+                    }
+                }
+                               
+                */
+                break;
+
+            case 2:
+                System.out.println("Bienvenido Empleado");
+                //Menu para registrar entrada y salida de autos
+                break;
+
+            case 3:
+                System.out.println("Bienvenido Cliente");
+                //Menu para ver el costo que genero su vehiculo
                 break;
 
             default:
