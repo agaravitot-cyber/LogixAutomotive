0017b7b2af36290f496a75f64cd36c5ba41814f7
JuanMartinezMarq <juanesmartinez200304@gmail.com>
2026-04-25 09:36:08 -0500

    Update Celda.java

--- app/src/main/java/parqueaderoapp/modelo/parqueadero/Celda.java
+++ app/src/main/java/parqueaderoapp/modelo/parqueadero/Celda.java
@@ -1,5 +1,40 @@
-package parqueaderoapp.modelo.parqueadero;
-
 public class Celda {
-    // TODO: generar la clase celda
+    private boolean espacioCarro;
+    private boolean espacioMoto;
+    private boolean espacioBici;
+
+    public Celda() {
+        this.espacioCarro = false;
+        this.espacioMoto = false;
+        this.espacioBici = false;
+    }
+
+    public boolean ocupa(String tipoVehiculo) {
+        switch (tipoVehiculo.toLowerCase()) {
+            case "carro":
+                if (!espacioCarro) {
+                    espacioCarro = true;
+                    return true;
+                }
+                return false;
+
+            case "moto":
+                if (!espacioMoto) {
+                    espacioMoto = true;
+                    return true;
+                }
+                return false;
+
+            case "bici":
+                if (!espacioBici) {
+                    espacioBici = true;
+                    return true;
+                }
+                return false;
+
+            default:
+                System.out.println("Tipo de vehículo no válido");
+                return false;
+        }
+    }
 }
